1fd435d549a82b8dd711fe09aca2dcd89d827007
Victor05102007 <victorciceri5@gmail.com>
2025-04-23 13:23:10 -0300

    terceiro commit

--- programação/scripts.js
+++ programação/scripts.js
@@ -1,27 +1,61 @@
-console.log("Java Conectado!");
-
 const search = document.getElementById("search");
-const cityInput = document.getElementById("local");
-const h1 = document.getElementById("id");
+const CityInput = document.getElementById("local");
+const title = document.getElementById("title");
 const img = document.getElementById("img");
-const temptop = document.getElementById("temptop");
-const tempmin = document.getElementById("tempmin");
-const tempmax = document.getElementById("tempmax");
-const umidade = document.getElementById("humidity");
+const temp_value = document.getElementById("temp_value");
+const temp_description = document.getElementById("temp_description");
+const temp_max = document.getElementById("temp_max");
+const temp_min = document.getElementById("temp_min");
+const umidade = document.getElementById("umidade");
+const vento = document.getElementById("vento");
+const papai = document.getElementById("papai");
 
 const processar = async (event) => {
   event.preventDefault();
-  const cityName = cityInput.Value;
-  const key = "2fbbd1f1ef92491b13726661ccc0982b";
 
-  const response = await fetch(
-    `https://api.apenweatgermap.org/data/2.5/weather?q=${encodeURIComponent(
-      cityName
-    )}&appid=${key}&units=metric&lang=pt_br`
-  );
-  const data = await response.json();
+  try {
+    const CityName = CityInput.value;
+    const key = "98c4f758ddeca463e9d54b376eabe66b";
 
-  console.log(data);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+        CityName
+      )}&appid=${key}&units=metric&lang=pt-br`
+    );
+
+    const data = await response.json();
+
+    img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    title.textContent = data.name;
+    temp_max.innerHTML = `${data.main.temp_max}<sup>C°</sup>`;
+    temp_min.innerHTML = `${data.main.temp_min}<sup>C°</sup>`;
+    temp_value.textContent = data.main.temp;
+    temp_description.textContent = data.weather[0].description;
+    umidade.textContent = `${data.main.humidity}%`;
+    vento.textContent = `${data.wind.speed}Km/h`;
+
+    papai.style.visibility = "visible";
+    papai.style.opacity = "1";
+    papai.style.height = "350px";
+    papai.style.overflow = "visible";
+
+    console.log(data);
+  } catch (error) {
+    console.log("Erro ao buscar dados", error);
+  }
 };
 
-search.addEventListen("submit", processar);
+console.log(
+  search,
+  CityInput,
+  title,
+  img,
+  temp_value,
+  temp_description,
+  temp_max,
+  temp_min,
+  umidade,
+  vento
+);
+
+search.addEventListener("submit", processar);
